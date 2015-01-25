c8b4536a5f99dc92711be38b6c4fd6d622c139c2
Janne Pakarinen <janne.pakarinen@helsinki.fi>
2015-01-25 16:17:11 +0200

    T2.4 asti

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,18 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
   has_many :ratings
+
+  def average_rating
+    sum = 0
+    avg = 0
+    ratings.each do |rating|
+      sum += rating.score
+    end
+    if ratings.empty?
+      avg = 0
+    else
+      avg = sum / ratings.count
+    end
+    avg
+  end
 end
