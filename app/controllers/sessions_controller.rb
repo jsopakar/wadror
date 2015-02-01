d302e5100f3eb7cdf5e85e66d1f2d21e8c9a5690
Janne Pakarinen <janne.pakarinen@helsinki.fi>
2015-02-01 19:35:54 +0200

    Login kertoo virheestä

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,20 @@ class SessionsController < ApplicationController
   def create
     # haetaan usernamea vastaava käyttäjä tietokannasta
     user = User.find_by username: params[:username]
+
+    if user.nil?
+      redirect_to :back, notice: "User #{params[:username]} doesn't exist!"
+    else
+      session[:user_id] = user.id
+      redirect_to user, notice: "Welcome back!"
+
+
+    end
+
     # talletetaan sessioon kirjautuneen käyttäjän id (jos käyttäjä on olemassa)
-    session[:user_id] = user.id if not user.nil?
+    #session[:user_id] = user.id if not user.nil?
     # uudelleen ohjataan käyttäjä omalle sivulleen
-    redirect_to user
+    #redirect_to user
   end
 
   def destroy
