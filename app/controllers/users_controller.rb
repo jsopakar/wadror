c771d6cdcc9ef08b95270edf3d54635065723d22
Janne Pakarinen <janne.pakarinen@helsinki.fi>
2015-02-01 23:42:41 +0200

    3.12

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -60,6 +60,7 @@ class UsersController < ApplicationController
   def destroy
     if @user == current_user
       @user.destroy
+      session[:user_id] = nil
       respond_to do |format|
         format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
         format.json { head :no_content }
