34916349c66ae82fe2bc7002788a396051bd1b5c
Janne Pakarinen <janne.pakarinen@helsinki.fi>
2015-02-08 22:47:49 +0200

    T4.5

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,7 +3,9 @@ ENV['RAILS_ENV'] ||= 'test'
 require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
+require 'capybara/rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
+
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -47,4 +49,16 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+
 end
+
+#include OwnTestHelper
+
+
+  def sign_in(credentials)
+    visit signin_path
+    fill_in('username', with:credentials[:username])
+    fill_in('password', with:credentials[:password])
+    click_button('Log in')
+  end
