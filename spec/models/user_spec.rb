39247210e3e9008757d07f9f7653ea337d3ef831
Janne Pakarinen <janne.pakarinen@helsinki.fi>
2015-02-08 18:11:17 +0200

    Ennen capybaraa

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe User do
+
   it "has the username correctly" do
     user = User.new username:"Pekka"
 
@@ -39,7 +40,8 @@ describe User do
   end
 
   describe "with a proper password" do
-    let(:user){ User.create username:"Pekka", password:"Secret1", password_confirmation:"Secret1" }
+    #let(:user){ User.create username:"Pekka", password:"Secret1", password_confirmation:"Secret1" }
+    let(:user){ FactoryGirl.create(:user) }
 
     it "is saved" do
       expect(user).to be_valid
@@ -47,16 +49,55 @@ describe User do
     end
 
     it "and with two ratings, has the correct average rating" do
-      rating = Rating.new score:10
-      rating2 = Rating.new score:20
-
-      user.ratings << rating
-      user.ratings << rating2
+      #rating = Rating.new score:10
+      #rating2 = Rating.new score:20
+      
+      user.ratings << FactoryGirl.create(:rating)
+      user.ratings << FactoryGirl.create(:rating2)
 
       expect(user.ratings.count).to eq(2)
       expect(user.average_rating).to eq(15.0)
     end
   end
 
+
+  describe "favorite beer" do
+    let(:user){FactoryGirl.create(:user) }
+
+    it "has method for determining one" do
+      user.should respond_to :favorite_beer
+    end
+
+    it "without ratings does not have one" do
+      expect(user.favorite_beer).to eq(nil)
+    end
+
+    it "is the only rated if only one rating" do
+      beer = create_beer_with_rating(10, user)
+
+      expect(user.favorite_beer).to eq(beer)
+    end
+
+    it "is the one with highest rating if several rated" do
+      create_beers_with_ratings(10, 20, 15, 7, 9, user)
+      best = create_beer_with_rating(25, user)
+
+      expect(user.favorite_beer).to eq(best)
+    end
+  end
+
+
+
+  def create_beer_with_rating(score, user)
+    beer = FactoryGirl.create(:beer)
+    FactoryGirl.create(:rating, score:score, beer:beer, user:user)
+    beer
+  end
+
+  def create_beers_with_ratings(*scores, user)
+    scores.each do |score|
+      create_beer_with_rating(score, user)
+    end
+  end
 
 end
