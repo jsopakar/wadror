48b154aecc37b2e6408e40bfbebd0b132eca7815
Janne Pakarinen <janne.pakarinen@helsinki.fi>
2015-02-14 16:35:57 +0200

    beermapping api lisätty

---
+++ lib/beermapping_api.rb
@@ -0,0 +1,19 @@
+class BeermappingApi
+  def self.places_in(city)
+    url = "http://beermapping.com/webservice/loccity/#{key}/"
+
+    response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
+    places = response.parsed_response["bmp_locations"]["location"]
+
+    return [] if places.is_a?(Hash) and places['id'].nil?
+
+    places = [places] if places.is_a?(Hash)
+    places.inject([]) do | set, place |
+      set << Place.new(place)
+    end
+  end
+
+  def self.key
+    "80ed203fc139f8ac8bae31460be8bdb9"
+  end
+end
