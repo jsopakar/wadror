cec11c1fd74c86004120914327343a78160dd237
Janne Pakarinen <janne.pakarinen@helsinki.fi>
2015-02-22 23:19:41 +0200

    T6.5:n osatoteutusta, ei valmis

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,6 +1,7 @@
 class RatingsController < ApplicationController
   def index
     @ratings = Rating.all
+    @recent_ratings = Rating.recent
   end
 
   def new
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -6,6 +6,8 @@ class Rating < ActiveRecord::Base
                                     less_than_or_equal_to: 50,
                                     only_integer: true }
 
+   scope :recent, -> { order(beer_id: :desc).limit(2) }
+
   def to_s
     "#{beer.name} #{score}"
   end
