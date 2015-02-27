0eca94ca62f5aa4f09a67418f7cbefd8e750aadf
Janne Pakarinen <janne.pakarinen@helsinki.fi>
2015-02-27 16:59:44 +0200

    Ennen Angulariin siirtymistä tehdyt jutut

--- Gemfile
+++ Gemfile
@@ -65,3 +65,5 @@ gem 'bootstrap-sass'
 group :development do
   gem 'rails_layout'
 end
+
+gem 'jquery-turbolinks'
--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,8 @@ Rails.application.routes.draw do
   resources :places, only: [:index, :show]
   post 'places', to:'places#search'
 
+  get 'beerlist', to: 'beers#list'
+
   root 'breweries#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
